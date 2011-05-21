feb621b960bf6ee6ea47edd14aed55ec1bd83f63
Frank <frank@heidjer.info>
2011-05-21 11:45:04 +0200

    Switch all box specific messages to Rjiffy::Box. Cleanup Rjiffy::Connection

--- lib/rjiffy/connection.rb
+++ lib/rjiffy/connection.rb
@@ -8,15 +8,6 @@ module Rjiffy
       self.class.base_uri "https://api.jiffybox.de/#{token}/#{API_VERSION}"
     end
 
-    def list_boxes
-      response = self.class.get("/jiffyBoxes")
-      response["result"]
-    end
-
-    def find(id)
-      response = self.class.get("/jiffyBoxes/#{id}")
-      Box.new(response["result"])
-    end
 
   end
 end
