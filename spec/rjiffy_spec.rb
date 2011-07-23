5aac89d35168cc080044e91553c8b57641c35d85
Frank <frank@heidjer.info>
2011-07-23 22:32:44 +0200

    Enable all tests

--- spec/rjiffy_spec.rb
+++ spec/rjiffy_spec.rb
@@ -3,12 +3,12 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 describe Rjiffy do
 
   describe "basic handling for jiffyboxes" do
-    #it "handles empty results" do
-      #FakeWeb.register_uri(:get, Rjiffy::Configuration.base_uri["/jiffyBoxes"].to_s, :body => fixture_file("empty_result.json"), :content_type => "application/json")
-      #box_list = Rjiffy.all
-      #box_list.class.should == Array
-      #box_list.should be_empty
-    #end
+    it "handles empty results" do
+      FakeWeb.register_uri(:get, Rjiffy::Configuration.base_uri["/jiffyBoxes"].to_s, :body => fixture_file("empty_result.json"), :content_type => "application/json")
+      box_list = Rjiffy.all
+      box_list.class.should == Array
+      box_list.should be_empty
+    end
 
     it "list all jiffyboxes" do
       FakeWeb.register_uri(:get, Rjiffy::Configuration.base_uri["/jiffyBoxes"].to_s, :body => fixture_file("successfull_requested_list.json"), :content_type => "application/json")
@@ -27,16 +27,16 @@ describe Rjiffy do
       responded_box.name.should == "Test"
     end
 
-    #it "list the backups for all available boxes" do
-      #FakeWeb.register_uri(:get, Rjiffy::Configuration.base_uri["/backups"].to_s, :body => fixture_file("backup_list.json"), :content_type => "application/json")
-      #available_backups = Rjiffy.backups
-      #available_backups.class.should == Array
-      #available_backups.count.should == 1
-      #backup = available_backups.first
-      #backup.daily.id.should == "1234567890abcdef1234567890abcdef"
-      #backup.weekly.created == 1234567890
-      #backup.biweekly.id == "34567890abcdef1234567890abcdef12"
-    #end
+    it "list the backups for all available boxes" do
+      FakeWeb.register_uri(:get, Rjiffy::Configuration.base_uri["/backups"].to_s, :body => fixture_file("backup_list.json"), :content_type => "application/json")
+      available_backups = Rjiffy.backups
+      available_backups.class.should == Array
+      available_backups.count.should == 1
+      backup = available_backups.first
+      backup.daily.id.should == "1234567890abcdef1234567890abcdef"
+      backup.weekly.created == 1234567890
+      backup.biweekly.id == "34567890abcdef1234567890abcdef12"
+    end
   end
 
   describe "Errors", :handle_errors => true do
