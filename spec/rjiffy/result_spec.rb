ae05a34899ffca1a67ddf8a3449e16181bbd8bdb
Frank <frank@heidjer.info>
2011-09-16 17:00:50 +0200

    More specs

--- spec/rjiffy/result_spec.rb
+++ spec/rjiffy/result_spec.rb
@@ -14,4 +14,13 @@ describe Rjiffy::Result do
     expect { Rjiffy::Result.new(error_response) }.to raise_error(Rjiffy::ApiResponseError, "Der von Ihnen uebergebene API-Token ist ungueltig.")
   end
 
+  it "returns a Hashie::Mash when result is a hash" do
+    Rjiffy::Result.new({"result" => {:test => "result test"}}).data.class.should == Hashie::Mash
+  end
+
+  it "returns the raw data unless result is a hash" do
+    Rjiffy::Result.new({"result" => true}).data.class.should == TrueClass
+    Rjiffy::Result.new({"result" => "whatever"}).data.class.should == String
+  end
+
 end
